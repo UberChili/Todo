d5965c2059267a390d18453525044941716c7dbf
Juan Andres Nuñez <nunezlopez.ja@gmail.com>
2025-01-05 20:23:13 -0600

    Something. Just making sure everything is up to date for tomorrow

--- main.go
+++ main.go
@@ -3,13 +3,41 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
 	taskPtr := flag.String("add", "", "A new task to add (use quotes for multiple words, e.g., -add \"Buy groceries\")")
+	taskList := flag.Bool("l", true, "Lists open tasks")
 	flag.Parse()
 
-	fmt.Println(*taskPtr)
+	// TODO Check if "configuration" has been set
+	if !checkConfig() {
+		fmt.Println("You have no tasks directory configured. Refer to documentation.")
+		return
+	}
 
-	fmt.Println("Hello there!")
+	if *taskPtr != "" {
+		// We do not necessarily list open tasks
+		*taskList = false
+
+		// TODO
+		// 1. Get today's date
+		// 2. Check if daily file with today's date as title already exists
+		// 3. If it doesn't exist, create file in correct directory
+		// 4. Open file
+		// 5. Get to end of File and write new task
+	}
+
+	// fmt.Println(*taskPtr)
+	// fmt.Println(*taskList)
+}
+
+func checkConfig() bool {
+	directory := os.Getenv("DAILIES_DIRECTORY")
+
+	if directory == "" {
+		return false
+	}
+	return true
 }
