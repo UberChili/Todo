30b0e768c501763ddd46f51f6e90bbea3c659d8d
Juan Andres Nuñez <nunezlopez.ja@gmail.com>
2025-01-08 16:02:10 -0600

    Fixed logic in main.go. Added a header for the tasklist

--- main.go
+++ main.go
@@ -3,20 +3,18 @@ package main
 import (
 	"flag"
 	"fmt"
-	"time"
-
 	"github.com/UberChili/todo/config"
 	"github.com/UberChili/todo/tasks"
+	"time"
 )
 
 func main() {
 	taskPtr := flag.String("add", "", "A new task to add (use quotes for multiple words, e.g., -add \"Buy groceries\").")
-	taskList := flag.Bool("l", true, "Lists open tasks.")
+	taskList := flag.Bool("l", false, "Lists open tasks.") // Changed default to false
 	delTask := flag.Int("r", 0, "Remove a task by specifing id.")
 	flag.Parse()
 
 	filename := time.Now().Format("2006-01-02")
-
 	directory, exists := config.ConfigExists()
 	if !exists {
 		fmt.Println("You have no tasks directory configured. Refer to documentation.")
@@ -25,32 +23,39 @@ func main() {
 
 	filename = config.FormatFilename(directory, filename)
 
-	tasklist, err := tasks.ReadOpenTasks(filename)
-	if err != nil {
-		fmt.Println("You have no open tasks! Free day!")
-		// return
-	}
-
-	// Add a new task
+	// Handle the -add flag
 	if *taskPtr != "" {
 		tasks.AddNewTask(filename, *taskPtr)
 		return
 	}
 
-	// Deleting a task, specified by id
+	// Handle the -r flag
 	if *delTask > 0 {
-		// Checking if valid task
-		if *delTask > len(tasklist) {
-			fmt.Printf("Task %d does not exist.", *delTask)
+		tasklist, err := tasks.ReadOpenTasks(filename)
+		if err != nil {
+			fmt.Println("No tasks to remove!")
 			return
 		}
-		// Remove the task
+
+		if *delTask > len(tasklist) {
+			fmt.Printf("Task %d does not exist.\n", *delTask)
+			return
+		}
+
 		tasks.CompleteTask(filename, *delTask)
 		return
 	}
 
-	// List open tasks, this is the default behavior when no argument or flag is given
-	if *taskList {
+	// List tasks if either no flags are provided or -l flag is explicitly set
+	if flag.NFlag() == 0 || *taskList {
+		tasklist, err := tasks.ReadOpenTasks(filename)
+		if err != nil {
+			fmt.Println("You have no open tasks! Free day!")
+			return
+		}
+
+		// fmt.Printf("id\tTask\n")
+		fmt.Printf("\033[1mid\tTask\033[0m\n")
 		config.ListOpenTasks(tasklist)
 		return
 	}
